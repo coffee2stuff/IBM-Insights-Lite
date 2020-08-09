0072f8c54ae67d4caac77dca55b0f098d606b218
Peter Aleksander Bizjak <peteralex.developer@gmail.com>
2020-08-09 13:40:25 +0200

    Bug fixes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as axios from 'axios';
+import { ProfileModel } from './models';
+
 export class IBMInsightsLite {
     private readonly version: string = '2017-10-13';
 
@@ -9,6 +12,22 @@ export class IBMInsightsLite {
         this.baseURL = base;
         this.apiKey = key;
         this.requestURL = this.constructURL();
+    }
+
+    async createRequest(contents: string): Promise<ProfileModel> {
+        return (
+            await axios.default.post<ProfileModel>(this.requestURL, contents, {
+                auth: {
+                    username: 'apikey',
+                    password: this.apiKey
+                },
+                responseType: 'json',
+                headers: {
+                    'Content-Type': 'text/plain',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            })
+        ).data;
     }
 
     private constructURL(): string {
